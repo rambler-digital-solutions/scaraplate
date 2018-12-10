01875ca1db79bc5cb552e39a5af172cd01ec43de
Kostya Esmukov <k.esmukov@rambler-co.ru>
2018-12-10 16:45:31 +0300

    Add 2 strategies (Overwrite and TemplateHash)

---
+++ src/scaraplate/strategies.py
@@ -0,0 +1,46 @@
+import abc
+import io
+from typing import BinaryIO, Optional
+
+
+class Strategy(abc.ABC):
+    def __init__(
+        self,
+        *,
+        target_contents: Optional[BinaryIO],
+        template_contents: BinaryIO,
+        template_commit_hash: str,
+    ) -> None:
+        self.target_contents = target_contents
+        self.template_contents = template_contents
+        self.template_commit_hash = template_commit_hash
+
+    @abc.abstractmethod
+    def apply(self) -> BinaryIO:
+        pass
+
+
+class Overwrite(Strategy):
+    def apply(self) -> BinaryIO:
+        return self.template_contents
+
+
+class TemplateHash(Strategy):
+    def apply(self) -> BinaryIO:
+        hash_comment = (
+            f"# scaraplate template commit hash: {self.template_commit_hash}\n"
+        ).encode("ascii")
+        if self.target_contents is not None:
+            target_text = self.target_contents.read()
+            if hash_comment in target_text:
+                # Hash hasn't changed -- keep the target.
+                self.target_contents.seek(0)
+                return self.target_contents
+
+        out_bytes = self.template_contents.read()
+        out_bytes += b"\n" + hash_comment
+        return io.BytesIO(out_bytes)
+
+
+# XXX setup.cfg
+# XXX .pylintrc
---
+++ tests/test_strategies.py
@@ -0,0 +1,54 @@
+import io
+from typing import BinaryIO, Optional
+
+import pytest
+
+from scaraplate.strategies import TemplateHash
+
+
+@pytest.mark.parametrize(
+    "template, target, out",
+    [
+        (
+            "hi!",
+            None,
+            "hi!\n# scaraplate template "
+            "commit hash: 1111111111111111111111111111111111111111\n",
+        ),
+        (
+            "hi!",
+            "ho!",
+            "hi!\n# scaraplate template "
+            "commit hash: 1111111111111111111111111111111111111111\n",
+        ),
+        (
+            "hi!",
+            "ho!\n# scaraplate template "
+            "commit hash: 1111111111111111111111111111111111111111\n",
+            "ho!\n# scaraplate template "
+            "commit hash: 1111111111111111111111111111111111111111\n",
+        ),
+        (
+            "hi!",
+            "ho!\n# scaraplate template "
+            "commit hash: 0000000000000000000000000000000000000000\n",
+            "hi!\n# scaraplate template "
+            "commit hash: 1111111111111111111111111111111111111111\n",
+        ),
+    ],
+)
+def test_template_hash(template, target, out):
+    if target is not None:
+        target_contents: Optional[BinaryIO] = io.BytesIO(target.encode())
+    else:
+        target_contents = None
+
+    template_contents = io.BytesIO(template.encode())
+
+    strategy = TemplateHash(
+        target_contents=target_contents,
+        template_contents=template_contents,
+        template_commit_hash="1111111111111111111111111111111111111111",
+    )
+
+    assert out == strategy.apply().read().decode()
