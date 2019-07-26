65de9e82332a7f04658f0dac225371229b4da555
Kostya Esmukov <kostya@esmukov.ru>
2019-07-26 22:51:54 +0300

    Fix RTD build
    Sphinx error:
    master file /home/docs/checkouts/readthedocs.org/user_builds/scaraplate/checkouts/next/docs/contents.rst not found

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,9 @@ extensions = [
     "sphinx.ext.intersphinx",
 ]
 
+# The master toctree document.
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
